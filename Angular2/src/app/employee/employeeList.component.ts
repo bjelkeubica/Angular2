fc033419f2c81d536b29cba7610f938d2a1da097
Nemanja Bijelovic <nemanja.bijelovic@msg-global.com>
2018-03-02 12:16:27 +0100

    Adding pipes to the table in employeeList.component.html file

--- Angular2/src/app/employee/employeeList.component.ts
+++ Angular2/src/app/employee/employeeList.component.ts
@@ -8,9 +8,9 @@
 
 export class EmployeeListComponent {
     employees: any[] = [
-        { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 600, dateOfBirth: '25/6/1988' },
-        { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 570.95, dateOfBirth: '25/6/1988' },
-        { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 530, dateOfBirth: '25/6/1988' },
-        { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 540.826, dateOfBirth: '25/6/1988' }
+        { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 600, dateOfBirth: '6/25/1988' },
+        { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 570.95, dateOfBirth: '3/3/1990' },
+        { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 530, dateOfBirth: '10/10/1989' },
+        { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 540.826, dateOfBirth: '9/23/1988' }
     ];
 }
