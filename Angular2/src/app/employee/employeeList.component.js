ec11939b3c3606c71c3907465f8b2d0f67470276
Nemanja Bijelovic <nemanja.bijelovic@msg-global.com>
2018-03-02 15:24:37 +0100

    Adding input and output properties in application

--- Angular2/src/app/employee/employeeList.component.js
+++ Angular2/src/app/employee/employeeList.component.js
@@ -5,23 +5,42 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var EmployeeListComponent = /** @class */ (function () {
     function EmployeeListComponent() {
+        //We will need this for radio buttons above table
+        this.selectedEmployeeCountRadioButton = 'All';
         this.employees = [
-            { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 600, dateOfBirth: '6/25/1988' },
-            { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 570.95, dateOfBirth: '3/3/1990' },
-            { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 530, dateOfBirth: '10/10/1989' },
-            { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 540.826, dateOfBirth: '9/23/1988' }
+            { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 850, dateOfBirth: '6/25/1988' },
+            { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 777.95, dateOfBirth: '3/3/1990' },
+            { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 735, dateOfBirth: '10/10/1989' },
+            { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 742.826, dateOfBirth: '9/23/1988' },
+            { code: 'emp105', name: 'Nancy', gender: 'Female', annualSalary: 643.826, dateOfBirth: '1/19/1992' }
         ];
     }
+    EmployeeListComponent.prototype.getTotalEmployeesCount = function () {
+        return this.employees.length;
+    };
+    EmployeeListComponent.prototype.getTotalMaleEmployeesCount = function () {
+        return this.employees.filter(function (e) { return e.gender === "Male"; }).length;
+    };
+    EmployeeListComponent.prototype.getTotalFemaleEmployeesCount = function () {
+        return this.employees.filter(function (e) { return e.gender === "Female"; }).length;
+    };
+    EmployeeListComponent.prototype.onEmployeeCountRadioButtonChange = function (selectedRadioButtonValue) {
+        this.selectedEmployeeCountRadioButton = selectedRadioButtonValue;
+    };
     EmployeeListComponent = __decorate([
         core_1.Component({
             selector: 'list-employee',
             templateUrl: 'app/employee/employeeList.component.html',
             styleUrls: ['app/employee/employee.component.css']
-        })
+        }),
+        __metadata("design:paramtypes", [])
     ], EmployeeListComponent);
     return EmployeeListComponent;
 }());
--- Angular2/src/app/employee/employeeList.component.ts
+++ Angular2/src/app/employee/employeeList.component.ts
@@ -7,10 +7,34 @@
 })
 
 export class EmployeeListComponent {
-    employees: any[] = [
-        { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 600, dateOfBirth: '6/25/1988' },
-        { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 570.95, dateOfBirth: '3/3/1990' },
-        { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 530, dateOfBirth: '10/10/1989' },
-        { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 540.826, dateOfBirth: '9/23/1988' }
-    ];
+    employees: any[];
+
+    constructor() {
+        this.employees = [
+            { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 850, dateOfBirth: '6/25/1988' },
+            { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 777.95, dateOfBirth: '3/3/1990' },
+            { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 735, dateOfBirth: '10/10/1989' },
+            { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 742.826, dateOfBirth: '9/23/1988' },
+            { code: 'emp105', name: 'Nancy', gender: 'Female', annualSalary: 643.826, dateOfBirth: '1/19/1992' }
+        ];
+    }
+
+    //We will need this for radio buttons above table
+    selectedEmployeeCountRadioButton: string = 'All';
+
+    getTotalEmployeesCount(): number {
+        return this.employees.length;
+    }
+
+    getTotalMaleEmployeesCount(): number {
+        return this.employees.filter(e => e.gender === "Male").length;
+    }
+
+    getTotalFemaleEmployeesCount(): number {
+        return this.employees.filter(e => e.gender === "Female").length;
+    }
+    
+    onEmployeeCountRadioButtonChange(selectedRadioButtonValue: string): void {
+        this.selectedEmployeeCountRadioButton = selectedRadioButtonValue;
+    }
 }
